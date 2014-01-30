dfb9bbeb796dfca143ddca77e20e33f6303cb15a
topack <topack_us@yahoo.com>
2014-01-30 16:44:27 +0100

    Update and rename Assets/Scripts/ICharacter.cs to Assets/Scripts/Interfaces/ICharacter.cs

--- Assets/Scripts/Interfaces/ICharacter.cs
+++ Assets/Scripts/Interfaces/ICharacter.cs
@@ -3,31 +3,31 @@ public interface ICharacter
     /// <summary>
     /// Name of the character
     /// </summary>
-    IName Name { get; set; };
+    IName Name { get; set; }
     /// <summary>
     /// Level of the character
     /// </summary>
-    ILevel { get; set; };
+    ILevel { get; set; }
     /// <summary>
     /// Faction of the character
     /// </summary>
-    IFaction Faction { get; set; };
+    IFaction Faction { get; set; }
     /// <summary>
     /// Attribute list of the character
     /// </summary>
-    IAttribute[] Attributes { get; set; };
+    IAttribute[] Attributes { get; set; }
     /// <summary>
     /// Spell List of the character
     /// </summary>
-    ISpell[] Spells { get; set; };
+    ISpell[] Spells { get; set; }
     /// <summary>
     /// Aura List of the character
     /// </summary>
-    IAura[] Auras { get; set; };
+    IAura[] Auras { get; set; }
     /// <summary>
     /// Reputation list of the character
     /// </summary>
-    IReputation[] Reputations { get; set; };
+    IReputation[] Reputations { get; set; }
     
     #region Unity
     /// <summary>
