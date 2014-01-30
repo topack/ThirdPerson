af57dd6eb5c375cdb1920205c03a541bb87b35b8
topack <topack_us@yahoo.com>
2014-01-30 15:50:35 +0100

    Update IAttribute.cs

--- Assets/Scripts/IAttribute.cs
+++ Assets/Scripts/IAttribute.cs
@@ -1,9 +1,28 @@
 public interface IAttribute
 {
-    string Name { get; set; };
+    /// <summary>
+    /// Name of the attribute
+    /// </summary>
+    IName Name { get; set; };
+    /// <summary>
+    /// Value of the attribute
+    /// </summary>
     float Value { get; set; };
+    /// <summary>
+    /// Maximum value of the attribute
+    /// </summary>
     float MaximumValue { get; set; };
     
-    void Add();
-    void UpdateValue();
+    /// <summary>
+    /// add the value to the attribute value
+    /// </summary>
+    void Add(int value);
+    /// <summary>
+    /// add the value to the attribute maximum value
+    /// </summary>
+    void AddMaximum(int value);
+    /// <summary>
+    /// Update the values of the attribute
+    /// </summary>
+    void UpdateValues();
 }
