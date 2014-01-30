b9ca533bcc604b065cf9db39e1336c6408c68205
topack <topack_us@yahoo.com>
2014-01-30 17:34:19 +0100

    Update IReputation.cs

--- Assets/Scripts/Interfaces/IReputation.cs
+++ Assets/Scripts/Interfaces/IReputation.cs
@@ -1,9 +1,13 @@
 public interface IReputation
 {
     /// <summary>
-    /// Name of the faction
+    /// Name of the reputation
     /// </summary>
     IName Name { get; set; }
+    /// <summary>
+    /// Type of reputation for the reputation
+    /// </summary>
+    ReputationType ReputationType { get; set; }
     /// <summary>
     /// Level of the reputation
     /// </summary>
