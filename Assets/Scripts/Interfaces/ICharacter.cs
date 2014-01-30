ca5114900593287497aea43aa7b7c78b39961af7
topack <topack_us@yahoo.com>
2014-01-30 18:01:00 +0100

    Update ICharacter.cs

--- Assets/Scripts/Interfaces/ICharacter.cs
+++ Assets/Scripts/Interfaces/ICharacter.cs
@@ -28,26 +28,4 @@ public interface ICharacter
     /// Reputation list of the character
     /// </summary>
     IReputation[] Reputations { get; set; }
-    
-    #region Unity
-    /// <summary>
-    /// called once when the script is attached to the object
-    /// </summary>
-    void Awake ();
-    
-    /// <summary>
-    /// called once when the script is enabled
-    /// </summary>
-    void Start();
-    
-    /// <summary>
-    /// called at fixed intervals
-    /// </summary>
-    void FixedUpdate ();
-    
-    /// <summary>
-    /// called every frame
-    /// </summary>
-    void Update ();
-    #enregion
 }
