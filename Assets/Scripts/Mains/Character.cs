8b0d0739f95e836aaed0554993de7a97a60b07c3
topack <topack_us@yahoo.com>
2014-01-30 18:01:35 +0100

    Update Character.cs

--- Assets/Scripts/Mains/Character.cs
+++ Assets/Scripts/Mains/Character.cs
@@ -3,4 +3,25 @@ using System.Collections;
 
 public class Character : MonoBehaviour, ICharacter
 {
+    #region Unity
+    /// <summary>
+    /// called once when the script is attached to the object
+    /// </summary>
+    void Awake ();
+    
+    /// <summary>
+    /// called once when the script is enabled
+    /// </summary>
+    void Start();
+    
+    /// <summary>
+    /// called at fixed intervals
+    /// </summary>
+    void FixedUpdate ();
+    
+    /// <summary>
+    /// called every frame
+    /// </summary>
+    void Update ();
+    #endregion
 }
