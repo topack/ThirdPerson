e49f180ef820ed69d9d4c9ce3a25d17484bb29ee
topack <topack_us@yahoo.com>
2014-01-30 12:00:36 +0100

    Create Character.cs

---
+++ Assets/Character.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+    // called once when the script is attached to the object
+    void Awake ()
+    }
+    }
+  
+    // called once when the script is enabled
+    void Start()
+    {
+    }
+  
+    // called at fixed intervals
+    void FixedUpdate ()
+    {
+    }
+    
+    // called every frame
+    void Update ()
+    {
+    }
+}
