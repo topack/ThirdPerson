308eef7ed1ad0f5f992ae0edb2c288e4c96510e5
topack <topack_us@yahoo.com>
2014-01-30 15:17:44 +0100

    Update ICharacter.cs

--- Assets/ICharacter.cs
+++ Assets/ICharacter.cs
@@ -3,25 +3,54 @@ using System.Collections;
 
 public class ICharacter : MonoBehaviour
 {
+    /// <summary>
+    /// Name of the character
+    /// </summary>
+    IName Name { get; set;};
+    /// <summary>
+    /// Level of the character
+    /// </summary>
+    ILevel { get; set;};
+    /// <summary>
+    /// Faction of the character
+    /// </summary>
+    IFaction Faction { get; set;};
+    /// <summary>
+    /// Attribute list of the character
+    /// </summary>
+    IAttribute[] Attributes { get; set;};
+    /// <summary>
+    /// Spell List of the character
+    /// </summary>
+    ISpell[] Spells { get; set;};
+    /// <summary>
+    /// Aura List of the character
+    /// </summary>
+    IAura[] Auras { get; set;};
+    /// <summary>
+    /// Reputation list of the character
+    /// </summary>
+    IReputation[] Reputations { get; set;};
+    
     #region Unity
-    // called once when the script is attached to the object
+    /// <summary>
+    /// called once when the script is attached to the object
+    /// </summary>
     void Awake ();
-  
-    // called once when the script is enabled
+    
+    /// <summary>
+    /// called once when the script is enabled
+    /// </summary>
     void Start();
-  
-    // called at fixed intervals
+    
+    /// <summary>
+    /// called at fixed intervals
+    /// </summary>
     void FixedUpdate ();
     
-    // called every frame
+    /// <summary>
+    /// called every frame
+    /// </summary>
     void Update ();
     #enregion
-    
-    Name Name { get; set;};
-    Level Level { get; set;};
-    Faction Faction { get; set;};
-    Attribute[] Attributes { get; set;};
-    Spell[] Spells { get; set;};
-    Aura[] Auras { get; set;};
-    Reputation[] Reputations { get; set;};
 }
