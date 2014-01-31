819af68f1be46a0c7bde66f5f6a843f18b27e505
topack <topack_us@yahoo.com>
2014-01-31 16:52:58 +0100

    Update Main.cs

--- Assets/Scripts/Mains/Main.cs
+++ Assets/Scripts/Mains/Main.cs
@@ -4,21 +4,29 @@ using System.Collections.Generic;
 public class Main : MonoBehaviour
 {
 	public Character Player;
+	public Icon Icon;
 	
-	public void Awake()
+	public void Start()
 	{
+		Icon = new GameObject("SpellIcon1", Icon) as Icon;
+		Icon.Player = Player;
+		Icon.Spell = new Spell(1);
 	}
 	
 	public void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.A))
 		{
+			Player.AddSpell(Icon.Spell);
+			Player.AddSpell(new SpellIce(2));
 		}
 		if(Input.GetKeyDown(KeyCode.Z))
 		{
+			Player.CastSpell(1);
 		}
 		if(Input.GetKeyDown(KeyCode.E))
 		{
+			Player.CastSpell(2);
 		}
 	}
 }
