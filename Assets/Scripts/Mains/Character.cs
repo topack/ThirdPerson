19ec334a9ea48d8d7c5383a50b9501a5ce3683eb
topack <topack_us@yahoo.com>
2014-01-31 16:52:48 +0100

    Update Character.cs

--- Assets/Scripts/Mains/Character.cs
+++ Assets/Scripts/Mains/Character.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Character : MonoBehaviour
 {
 	#region Declaration
-	public Attribute Health;
-	public List<Spell> Spells;
+	public int Health;
+	public List<Spell> Spells
+	
+	public GameObject Target;
 	#endregion
+	
+	public AddSpell(Spell spell)
+	{
+		if(spell != null)
+		{
+			Spells.Add(spell);
+		}
+	}
+	
+	public CastSpell(int spellId)
+	{
+		Spell cast = Spells.FirstOrDefault(p => p.SpellId == spellId);
+		if(cast != null)
+		{
+			spell.Cast(this.GameObject, Target);
+		}
+	}
 }
