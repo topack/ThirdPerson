1de35a0d07115c27fad43b63d270ac8e3b944f1f
topack <topack_us@yahoo.com>
2014-01-31 15:57:15 +0100

    Update Main.cs

--- Assets/Scripts/Mains/Main.cs
+++ Assets/Scripts/Mains/Main.cs
@@ -3,5 +3,22 @@ using System.Collections.Generic;
 
 public class Main : MonoBehaviour
 {
-
+	public Character Player;
+	
+	public void Awake()
+	{
+	}
+	
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+		}
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+		}
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+		}
+	}
 }
