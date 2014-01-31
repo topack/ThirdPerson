e7946578c45af7fbd36417f12fed42e95cf21b52
topack <topack_us@yahoo.com>
2014-01-31 14:30:58 +0100

    Update Character.cs

--- Assets/Scripts/Mains/Character.cs
+++ Assets/Scripts/Mains/Character.cs
@@ -4,43 +4,7 @@ using System.Collections.Generic;
 public class Character : MonoBehaviour
 {
 	#region Declaration
-	public int Value;
-	public List<GameObject> Spells;
+	public int Health;
+	public List<GameObject> SpellObjects;
 	#endregion
-	
-    #region Unity
-    /// <summary>
-    /// called once when the script is attached to the object
-    /// </summary>
-    void Awake ()
-	{
-		Spell spell = Spells[0].GetComponent<Spell>();
-		spell.CastSpell(this.gameObject);
-		Debug.Log(this.Value);
-	}
-    
-    /// <summary>
-    /// called once when the script is enabled
-    /// </summary>
-    void Start()
-	{
-
-	}
-    
-    /// <summary>
-    /// called at fixed intervals
-    /// </summary>
-    void FixedUpdate ()
-	{
-
-	}
-    
-    /// <summary>
-    /// called every frame
-    /// </summary>
-    void Update ()
-	{
-
-	}
-    #endregion
 }
