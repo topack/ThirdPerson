3155ce42f90d5753e4816eb9dff034933ffab7cc
topack <topack_us@yahoo.com>
2014-02-03 17:48:11 +0100

    Update DisplayIconSpell.cs

--- Assets/Scripts/Mains/DisplayIconSpell.cs
+++ Assets/Scripts/Mains/DisplayIconSpell.cs
@@ -1,1 +1,27 @@
-d
+using UnityEngine;
+using System.Collections;
+
+public class DisplayIconSpell : MonoBehaviour
+{
+        public Vector2 Position;
+        public Vector2 Size;
+        
+        private Character Character;
+        
+        void OnAwake()
+        {
+                Character = this.GameObject.GetComponent<Character>();
+        }
+        
+        void OnGUI()
+        {
+                //TODO display 
+                if(Character ! =null)
+                {
+                        foreach(SpellPrefab spellPrefab in Character.Auras)
+                        {
+                                
+                        }
+                }
+        }
+}
