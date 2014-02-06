457efb4db004dd38b7828d3d4a5194e56d16fac9
topack <topack_us@yahoo.com>
2014-02-06 10:53:24 +0100

    Change to GuiAuraContainer and GuiAura
    GuiAura just display the content.
    GuiAuraContainer manage all the auras in the container.

--- Assets/Scripts/Gui/GuiAura.cs
+++ Assets/Scripts/Gui/GuiAura.cs
@@ -4,67 +4,24 @@ using System.Collections;
 [RequireComponent(typeof(GUITexture))]
 [RequireComponent(typeof(GUIText))]
 public class GuiAura : MonoBehaviour
-{	
-	public Texture DefaultTexture;
-
-	private Vector2 size;
-	private Vector2 positionOffset;
-	private bool followTransform;
-	private Vector2 positionOnScreen;
-	private Transform transformToFollow;
-	private SpellPrefab spellPrefab;
+{
 	private GUITexture guiAuraTexture;
 	private GUIText guiLabel;
 
 	void Awake()
 	{
 		guiAuraTexture = this.gameObject.GetComponent<GUITexture>();
+		guiLabel.transform.position = Vector3.zero;
 		guiLabel = this.gameObject.GetComponent<GUIText>();
+		guiLabel.transform.position = new Vector3(0,0,1);
 	}
 
-	public void Init(Vector2 size, Vector2 positionOffset, bool followTransform, Vector2 positionOnScreen, Transform transformToFollow, SpellPrefab spellPrefab)
+	public void UpdateGuiAura(Vector2 position, Vector2 size, Texture texture, string text)
 	{
-		this.size = size;
-		this.positionOffset = positionOffset;
-		this.followTransform = followTransform;
-		this.positionOnScreen = positionOnScreen;
-		this.transformToFollow = transformToFollow;
-		this.spellPrefab = spellPrefab;
-		if (this.spellPrefab == null || this.spellPrefab.IconTexture == null)
-		{
-			this.guiAuraTexture.texture = Main.DefaultTexture;
-		}
-		else
-		{
-			this.guiAuraTexture.texture = this.spellPrefab.IconTexture;
-		}
-	}
-
-	void FixedUpdate ()
-	{
-		if (followTransform && transformToFollow != null)
-		{
-			screenPos = Camera.main.WorldToScreenPoint(transformToFollow.position);
-		}
-		else
-		{
-			screenPos = Vector3.zero;
-		}
-
-		if(!followTransform)
-		{
-			screenPos = positionOnScreen;
-		}
-
-		guiAuraTexture.pixelInset = new Rect(screenPos.x + positionOffset.x, screenPos.y + positionOffset.y, size.x, size.y);
-		guiLabel.pixelOffset = new Vector2(screenPos.x + positionOffset.x + 2, screenPos.y + positionOffset.y + 2);
-
-		guiLabel.text = Mathf.CeilToInt(spellPrefab.Duration - spellPrefab.TotalDuration).ToString();
-	}
-
-	public void UpdatePositionOffset(Vector2 positionOffset)
-	{
-		this.positionOffset = positionOffset;
+		guiAuraTexture.pixelInset = new Rect(position.x, position.y, size.x, size.y);
+		guiLabel.pixelOffset = new Vector2(position.x + (size.x / 2), position.y + (size.y / 2));
+		guiAuraTexture.texture = texture;
+		guiLabel.text = text;
 	}
 
 	public void UpdateText(string text)
--- Assets/Scripts/Gui/GuiAuraContainer.cs
+++ Assets/Scripts/Gui/GuiAuraContainer.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 
 public class GuiAuraContainer : MonoBehaviour
 {
+	/// <summary>
+	/// Number of aura to display horizontaly and verticaly.
+	/// </summary>
 	public Vector2 NumberOfAura;
-	public Vector2 IconSize;
+	/// <summary>
+	/// Size of the texture for the aura.
+	/// </summary>
+	public Vector2 AuraSize;
+	/// <summary>
+	/// If true the container will follow its transform for 3d render (exemple character plate).
+	/// </summary>
 	public bool FollowTransform;
+	/// <summary>
+	/// Position on screen When not following the its transform.
+	/// </summary>
 	public Vector2 PositionOnScreen;
-	
-	private List<AuraIcon> auraIcons = new List<AuraIcon>();
+	/// <summary>
+	/// Display the auras of this Character.
+	/// </summary>
 	public Character character;
+
+	private List<AuraInContainer> aurasInContainer = new List<AuraInContainer>();
 
 	public void Awake()
 	{
@@ -19,65 +34,72 @@ public class GuiAuraContainer : MonoBehaviour
 
 	public void FixedUpdate()
 	{
-
+		UpdateAuras();
 	}
 
 	public void AddAura(SpellPrefab spellPrefab)
 	{
+		if (aurasInContainer.Count >= (NumberOfAura.x * NumberOfAura.y))
+		{
+			return;
+		}
+
 		Object prefabGuiAura = Resources.Load(Main.PrefabGuiAura);
 		if (prefabGuiAura != null)
 		{
 			GameObject iconObj = GameObject.Instantiate(prefabGuiAura, Vector3.zero, Quaternion.identity) as GameObject;
 			iconObj.transform.parent = this.transform;
 			GuiAura guiAura = iconObj.GetComponent<GuiAura>();
-			guiAura.Init(IconSize, AuraPosition(auraIcons.Count), FollowTransform, PositionOnScreen, this.transform, spellPrefab);
-
-			auraIcons.Add(new AuraIcon(guiAura, spellPrefab));
+			aurasInContainer.Add(new AuraInContainer(guiAura, spellPrefab));
 		}
 	}
 
 	public void RemoveAura(SpellPrefab spellPrefab)
 	{
-		GameObject.Destroy(auraIcons.Find(p => p.SpellPrefab == spellPrefab).GuiAura.gameObject);
-		auraIcons.RemoveAll(p => p.SpellPrefab == spellPrefab);
-
-		UpdateAuraPosition();
+		GameObject.Destroy(aurasInContainer.Find(p => p.SpellPrefab == spellPrefab).GuiAura.gameObject);
+		aurasInContainer.RemoveAll(p => p.SpellPrefab == spellPrefab);
 	}
 
 	#region private
+	private void UpdateAuras()
+	{
+		Vector2 aurasContainerPosition = PositionOnScreen;
+		if (FollowTransform)
+		{
+			aurasContainerPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+		}
+
+		int nbAura = 0;
+		foreach (AuraInContainer auraInContainer in aurasInContainer)
+		{
+			auraInContainer.GuiAura.UpdateGuiAura(
+				aurasContainerPosition + AuraPosition(nbAura),
+				AuraSize,
+				auraInContainer.SpellPrefab.IconTexture,
+				Mathf.CeilToInt(auraInContainer.SpellPrefab.Duration - auraInContainer.SpellPrefab.TotalDuration).ToString()
+				);
+
+			nbAura++;
+		}
+	}
+
 	private Vector2 AuraPosition(int nbAura)
 	{
-		float x = 0;
-		float y = 0;
-
 		int nbRow = Mathf.FloorToInt(nbAura / NumberOfAura.x);
 		float auraInRow = nbAura - (nbRow * NumberOfAura.x);
 
-		y = IconSize.y * nbRow;
-		x = auraInRow * IconSize.x;
-
-		return new Vector2(x, y);
-	}
-
-	private void UpdateAuraPosition()
-	{
-		int i = 0;
-		foreach (AuraIcon auraIcon in auraIcons)
-		{
-			auraIcon.GuiAura.UpdatePositionOffset(AuraPosition(i));
-			i++;
-		}
+		return new Vector2(AuraSize.y * nbRow, auraInRow * AuraSize.x);
 	}
 	#endregion
 
-	public class AuraIcon
+	public class AuraInContainer
 	{
-		public AuraIcon(GuiAura ia, SpellPrefab sp)
+		public AuraInContainer(GuiAura ia, SpellPrefab sp)
 		{
 			GuiAura = ia;
 			SpellPrefab = sp;
 		}
-		
+
 		public GuiAura GuiAura;
 		public SpellPrefab SpellPrefab;
 	}
